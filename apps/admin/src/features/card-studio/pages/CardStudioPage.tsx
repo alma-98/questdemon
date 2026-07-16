b359df35f1679561064dd90cde6e95def9499232
alma-98 <alma.budsteddy88@gmail.com>
2026-07-17 03:03:53 +0700

    Part C - Card Studio Core

---
+++ apps/admin/src/features/card-studio/pages/CardStudioPage.tsx
@@ -0,0 +1,29 @@
+
+import CardForm from "../components/CardForm";
+import CardTable from "../components/CardTable";
+
+export default function CardStudioPage(){
+
+return (
+
+<div>
+
+<h1>
+🃏 Quest Demon Card Studio
+</h1>
+
+
+<CardForm/>
+
+
+<hr/>
+
+
+<CardTable/>
+
+
+</div>
+
+)
+
+}
---
+++ apps/admin/src/features/card-studio/types/card.ts
@@ -0,0 +1,25 @@
+export interface QuestCard {
+
+id:string;
+
+cardCode:string;
+
+name:string;
+
+type:string;
+
+race:string;
+
+element:string;
+
+rarity:string;
+
+price:number;
+
+playCount:number;
+
+imageUrl:string;
+
+description:string;
+
+}
