d05c2ced979e04cc590e52a70410139f8fc1afa6
alma-98 <alma.budsteddy88@gmail.com>
2026-07-17 02:42:07 +0700

    Part 2 - Card Engine

---
+++ scripts/generate-cards.js
@@ -0,0 +1,2 @@
+console.log("Quest Demon Card Generator");
+console.log("Generate 600 Cards");
---
+++ scripts/generate-serial.js
@@ -0,0 +1,6 @@
+const fs=require("fs");
+
+for(let i=1;i<=600;i++){
+ const code="QD-"+String(i).padStart(4,"0");
+ console.log(code+"-000001");
+}
